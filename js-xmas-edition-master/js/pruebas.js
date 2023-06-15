0e297f13b01a3c612f29075ba09b9602716171f5
milesolaron <127025206+milesolaron@users.noreply.github.com>
2023-06-15 09:58:55 -0300

    Update pruebas.js
    Mejoré el código de las pruebas

--- js-xmas-edition-master/js/pruebas.js
+++ js-xmas-edition-master/js/pruebas.js
@@ -1,56 +1,63 @@
 function probarValidarNombre() {
-  console.assert(
-      validarNombre('') === 'Este campo debe tener al menos 1 caracter',
-      'Validar nombre no validó que el nombre no sea vacío',
-  );
-
-  console.assert(
+    console.assert(
+      validarNombre('') === 'El campo nombre debe tener al menos 1 caracter',
+      'Validar nombre no validó que el nombre no esté vacío'
+    );
+  
+    console.assert(
       validarNombre(
-          '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
-      'Este campo debe tener menos de 50 caracteres',
-      'Validar nombre no validó que el nombre sea menor a 50 caracteres',
-  );
-
-  console.assert(
-    validarNombre("Milena") === "",
-    "validarNombre falló con un nombre válido"
-);
-}
-
-
-function probarValidarCiudad() {
-    console.assert(
-        validarCiudad("Buenos Aires") === "",
-        "Validar ciudad no funcionó con un nombre de ciudad válido"
+        '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111'
+      ) === 'El campo debe tener menos de 50 caracteres',
+      'Validar nombre no validó que el nombre sea menor a 50 caracteres'
     );
-
+  
     console.assert(
-        validarCiudad("") === "El campo ciudad no puede estar vacío",
-        "Validar ciudad no mostró un error cuando la ciudad está vacía"
+      validarNombre('Milena') === '',
+      'Validar nombre falló con un nombre válido'
     );
-}
-
-function probarValidarDescripcionRegalo() {
-
-console.assert(
-    validarDescripcionRegalo("") ===
-    "El campo descripción no puede estar vacío",
-    "La función validar descripción regalo no validó que el campo no esté vacío"
-);
-
-console.assert(
-    validarDescripcionRegalo("jdejiejejdejfirjfrijfiedejdeodjejdeodjejodedejoedjeodjededoedoedejdeodedoedejdjejdoejdejdjeojdoejdedededededededededededeedededededededede"
-    ) === "El campo descripción es muy largo",
-    "La función validar descripción regalo no validó que el campo no sea muy largo"
-);
-
-console.assert(
-    validarDescripcionRegalo("Regalo") === "",
-    "La función validar descripción regalo no funcionó con una descripción correcta"
-
-);
-}
-
-probarValidarNombre();
-probarValidarCiudad();
-probarValidarDescripcionRegalo();
+  
+    console.assert(
+      validarNombre('1231434') === 'El campo nombre solo acepta letras',
+      'Validar nombre no validó que el nombre solo tenga letras'
+    );
+  }
+  
+  function probarValidarCiudad() {
+    console.assert(
+      validarCiudad('Buenos Aires') === '',
+      'Validar ciudad no funcionó con un nombre de ciudad válido'
+    );
+  
+    console.assert(
+      validarCiudad('') === 'El campo ciudad no puede estar vacío',
+      'Validar ciudad no mostró un error cuando la ciudad está vacía'
+    );
+  }
+  
+  function probarValidarDescripcionRegalo() {
+    console.assert(
+      validarDescripcionRegalo('') === 'El campo descripción no puede estar vacío',
+      'La función validar descripción regalo no validó que el campo no esté vacío'
+    );
+  
+    console.assert(
+      validarDescripcionRegalo(
+        'jdejiejejdejfirjfrijfiedejdeodjejdeodjejodedejoedjeodjededoedoedejdeodedoedejdjejdoejdejdjeojdoejdedededededededededededeedededededededede'
+      ) === 'El campo descripción es muy largo',
+      'La función validar descripción regalo no validó que el campo no sea muy largo'
+    );
+  
+    console.assert(
+      validarDescripcionRegalo('Regalo') === '',
+      'La función validar descripción regalo no funcionó con una descripción correcta'
+    );
+  
+    console.assert(
+      validarDescripcionRegalo(',.,.,.') === 'El campo descripción solo puede contener números y letras',
+      'La función validar descripción regalo no validó que solo tenga números y letras'
+    );
+  }
+  
+  probarValidarNombre();
+  probarValidarCiudad();
+  probarValidarDescripcionRegalo();
